728862e3770e0973bd7dc6224605674c649713b5
Vinh Son <sonpv07@gmail.com>
2024-05-13 14:16:56 +0700

    first commit

---
+++ src/theme/colors.js
@@ -0,0 +1,24 @@
+export const COLORS = {
+  black: "#000000",
+  white: "#FFFFFF",
+  red: "#FF0000",
+
+  // GREEN
+  darkGreenText: "#2A9083",
+  darkGreenBackground: "rgba(42, 144, 131, 0.25)",
+  chipGreenBackGround: "rgba(0, 210, 132, 0.13)",
+  chipGreenText: "#00D284",
+  lightGreenText: "#03AF00",
+  lightGreenBackground: "#E0F5E0",
+
+  // ORANGE
+  orangeText: "#FF8A00",
+  orangeBackground: "rgba(255, 138, 0, 0.1)",
+  lightOrange: "#FFDCB2",
+
+  // GREY
+  greyBackground: "rgba(117, 117, 117, 0.1)",
+  greyText: "#757575",
+  lightGreyBorder: "#F1F1F1",
+  darkGreyBorder: "#C7C7C7",
+};
---
+++ src/theme/fonts.js
@@ -0,0 +1,18 @@
+export const SIZE = {
+  size_10: 10,
+  size_12: 12,
+  size_14: 14,
+  size_16: 16,
+  size_18: 18,
+};
+
+export const TYPE = {
+  quicksand: "Quicksand",
+};
+
+export const WEIGHT = {
+  weight_400: 400,
+  weight_500: 500,
+  weight_600: 600,
+  weight_700: 700,
+};
