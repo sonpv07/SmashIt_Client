3b03e6062aa4a64e38e510e9b0dd6be70b1be539
Vinh Son <sonpv07@gmail.com>
2024-05-20 17:03:58 +0700

    Update Chip Component

--- src/components/Atoms/Chip.jsx
+++ src/components/Atoms/Chip.jsx
@@ -3,43 +3,96 @@ import { StyleSheet, Touchable, TouchableOpacity, View } from "react-native";
 import { Text } from "react-native";
 import { COLORS } from "../../theme/colors";
 import { SIZE } from "../../theme/fonts";
+import VectorIcon from "./VectorIcon";
 
-const Chip = (props) => {
+const Chip = ({
+  backgroundColor,
+  borderColor,
+  text,
+  textColor,
+  textFamily,
+  action,
+  chipType,
+}) => {
   const [status, setStatus] = useState(false);
-  const isChosenBg =
-    status === false
-      ? {
-          backgroundColor: COLORS.white,
-          borderColor: COLORS.darkGreyBorder,
-        }
-      : {
-          backgroundColor: COLORS.darkGreenText,
-          borderColor: COLORS.white,
-        };
-  const isChosen = status === false ? COLORS.black : COLORS.white;
+  // const isChosenBg =
+  //   status === false
+  //     ? {
+  //         backgroundColor: COLORS.white,
+  //         borderColor: COLORS.darkGreyBorder,
+  //       }
+  //     : {
+  //         backgroundColor: COLORS.darkGreenText,
+  //         borderColor: COLORS.white,
+  //       };
+  // const isChosen = status === false ? COLORS.black : COLORS.white;
   return (
-    <View style={styles.outline}>
-      <TouchableOpacity
-        style={[styles.chip, isChosenBg]}
-        onPress={() => setStatus(!status)}
-      >
-        <Text style={{color: isChosen}}>{props.text}</Text>
-      </TouchableOpacity>
+    <View style={[styles.outline]}>
+      {chipType === "button" ? (
+        <TouchableOpacity
+          style={[
+            styles.chip,
+            { backgroundColor: backgroundColor, borderColor: borderColor },
+          ]}
+          // onPress={() => setStatus(!status)}
+        >
+          <Text style={{ color: textColor, fontFamily: textFamily }}>
+            {text}
+          </Text>
+
+          {action && (
+            <TouchableOpacity style={styles.close} onPress={action}>
+              <VectorIcon.AntDesign name="close" size={10} />
+            </TouchableOpacity>
+          )}
+        </TouchableOpacity>
+      ) : (
+        <View
+          style={[
+            styles.chip,
+            { backgroundColor: backgroundColor, borderColor: borderColor },
+          ]}
+        >
+          <Text style={{ color: textColor, fontFamily: textFamily }}>
+            {text}
+          </Text>
+
+          {action && (
+            <TouchableOpacity style={styles.close} onPress={action}>
+              <VectorIcon.AntDesign name="close" size={10} />
+            </TouchableOpacity>
+          )}
+        </View>
+      )}
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-    outline: {
-        display: 'flex',
-        alignItems: 'baseline',
-    },
+  outline: {
+    display: "flex",
+    alignItems: "baseline",
+    position: "relative",
+    marginTop: 5,
+  },
   chip: {
     paddingVertical: 10,
     paddingHorizontal: 20,
-    fontSize: SIZE.size_16,
+    fontSize: SIZE.size_14,
     borderRadius: 20,
     borderWidth: 1,
+  },
+
+  close: {
+    width: 17,
+    height: 17,
+    borderRadius: 8,
+    position: "absolute",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#DFE1E7",
+    right: -3,
+    top: -5,
   },
 });
 
