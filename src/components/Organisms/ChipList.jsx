01b4d72d133a1af9d67b64829bf9bc4e0f080ed8
Vinh Son <sonpv07@gmail.com>
2024-05-21 22:01:42 +0700

    Update Chip Component

--- src/components/Organisms/ChipList.jsx
+++ src/components/Organisms/ChipList.jsx
@@ -2,20 +2,45 @@ import React from "react";
 import Chip from "../Atoms/Chip";
 import { FlatList, ScrollView, StyleSheet, View } from "react-native";
 
-const ChipList = () => {
-  const data = [
-    "Long Thạnh Mỹ",
-    "Long Phước",
-    "Linh Đông",
-    "An Khánh",
-    "Bình Trưng Đông",
-    "Tăng Nhơn Phú B",
-  ];
+const ChipList = ({
+  dataList,
+  backgroundColor,
+  borderColor,
+  textColor,
+  textFamily,
+  action,
+  chipType,
+}) => {
+  // const data = [
+  //   "Long Thạnh Mỹ",
+  //   "Long Phước",
+  //   "Linh Đông",
+  //   "An Khánh",
+  //   "Bình Trưng Đông",
+  //   "Tăng Nhơn Phú B",
+  // ];
   return (
-    <ScrollView horizontal showsHorizontalScrollIndicator={false} style={styles.chipList}>
-        {data.map((locate, index )=> {
-            return <Chip key={index} text={locate} />
-        })}
+    <ScrollView
+      contentContainerStyle={{
+        flexDirection: "row",
+        flexWrap: "wrap",
+        columnGap: 5,
+        rowGap: 10,
+      }}
+    >
+      {dataList.map((item, index) => (
+        <View key={index}>
+          <Chip
+            borderColor={borderColor}
+            text={item}
+            backgroundColor={backgroundColor}
+            textFamily={textFamily}
+            textColor={textColor}
+            action={action}
+            chipType={chipType}
+          />
+        </View>
+      ))}
     </ScrollView>
   );
 };
