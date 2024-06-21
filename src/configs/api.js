434a040aa74aea8b58bd47903e842140b08198ad
Vinh Son <sonpv07@gmail.com>
2024-06-21 16:59:52 +0700

    Change API URL

--- src/configs/api.js
+++ src/configs/api.js
@@ -1,6 +1,6 @@
 //ngrok http 5036
 //port 5036
 //https://2db7-42-112-81-13.ngrok-free.app/swagger/index.html
-API_URL_ENV = "http://smashit.com.vn";
+API_URL_ENV = "http://api.smashit.com.vn";
 
 export default API_URL_ENV;
